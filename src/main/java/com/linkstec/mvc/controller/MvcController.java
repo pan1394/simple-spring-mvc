1414f1e200332e2a4e81204273f9bdf9cfc4fda0
panyl <panyl@SZ-GADZ050497.links.sz>
2019-05-21 20:02:57 +0800

    init

---
+++ src/main/java/com/linkstec/mvc/controller/MvcController.java
@@ -0,0 +1,42 @@
+package com.linkstec.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.linkstec.mvc.exception.MvcException;
+
+@Controller
+public class MvcController {
+
+    @GetMapping("/hello")
+    public String hello(){        
+        return "hello";
+    }
+    
+    @GetMapping("/user/{id}")
+    public String dosth(@PathVariable("id") String id, Model model){   
+    	if(!StringUtils.isEmpty(id)) {
+    		if(id.equals("1")) {
+    			model.addAttribute("user", "jack ma");
+    		}else if(id.equals("2")) {
+    			throw new MvcException("没有此用户");
+    		}else {
+    			int i = 3/0;
+    		}
+    	}
+        return "user";
+    }
+    
+    
+    @GetMapping("/error")
+    public String error(){        
+        return "error";
+    }
+}
